043c4a50a4d4dd6301bebe0ead4d3a79789087db
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2022-06-17 00:19:42 +0530

    Add files via upload

--- GetWeb.js
+++ GetWeb.js
@@ -1,8 +1,10 @@
 document.write('<script src="swiper.js"><\/script>');
+
 function get(){
 var copyText = document.getElementById("alert");
 alert(copyText.value);
 } 
+
 document.getElementById("nav-toggler").addEventListener("click", function(){ 
 const x = document.getElementById("navbar");
 if(x.style.maxHeight === "2000px") {
@@ -15,3 +17,9 @@ x.style.transition ="all 1s";
 }); 
 
  
+document.addEventListener('mouseup', function(e) {
+    var container = document.getElementById('navbar');
+    if (!container.contains(e.target)) {
+        container.style.maxHeight ="60px";		
+container.style.transition ="all 0.25s";
+    }});				
